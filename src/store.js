e2f625c717b16814f99f3ecfffdf41e146b6a465
Michael Cavalea <callmecavs@gmail.com>
2016-01-14 11:37:35 -0500

    add API proto

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import knot from 'knot.js'
 
 export default (namespace) => {
   const store = window.localStorage
+  let local = toObject(store.getItem(namespace)) || {}
 
   const instance = knot({
     get: get,
@@ -12,16 +13,30 @@ export default (namespace) => {
 
   return instance
 
-  function get() {
+  // helpers
 
+  function toObject(input) {
+    return JSON.parse(input)
   }
 
-  function set() {
-
+  function toString(input) {
+    return JSON.stringify(input)
   }
 
-  function remove() {
+  // API
 
+  function get(name) {
+    return local[name] || null
+  }
+
+  function set(name, value) {
+    local[name] = value
+    store.setItem(namespace, toString(local))
+  }
+
+  function remove(name) {
+    delete local[name]
+    store.setItem(namespace, toString(local))
   }
 
   function clear() {
