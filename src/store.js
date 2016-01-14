cb5a628569329768810343999c88e5afdecca002
Michael Cavalea <callmecavs@gmail.com>
2016-01-14 11:01:55 -0500

    add skeleton, clear method

--- src/store.js
+++ src/store.js
@@ -1,5 +1,30 @@
-export default class Store {
-  constructor(options = {}) {
+import knot from 'knot.js'
 
+export default (namespace) => {
+  const store = window.localStorage
+
+  const instance = knot({
+    get: get,
+    set: set,
+    remove: remove,
+    clear: clear
+  })
+
+  return instance
+
+  function get() {
+
+  }
+
+  function set() {
+
+  }
+
+  function remove() {
+
+  }
+
+  function clear() {
+    store.removeItem(namespace)
   }
 }
