22a216167027c9b8d15579f9c030e1791726e287
callmecavs <callmecavs@gmail.com>
2017-01-08 20:36:00 -0500

    remove gulpfile, replace with rollup config

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+
+const info = require('./package.json')
+
+const config = {
+  entry: 'src/stockpile.js',
+  plugins: [
+    resolve(),
+    babel()
+  ],
+  targets: [
+    {
+      dest: info.main,
+      format: 'umd',
+      moduleName: 'Stockpile'
+    }, {
+      dest: info.module,
+      format: 'es'
+    }
+  ]
+}
+
+export default config
